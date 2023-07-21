1e32344ffa8a05dd5cdbd4edb1192a809c7811eb
Tanmay2510 <comparisionthing@gmail.com>
2023-07-21 22:49:55 +0530

    cookies5

--- middleware.js
+++ middleware.js
@@ -4,15 +4,18 @@ export default function middleware(req,res){
     req.cookies.get('sessionId') ?
     req.cookies.get('sessionId').value
     : null
+    console.log(req.headers.get('referer'))
     let id = 
     req.cookies.get('userId') ?
     req.cookies.get('userId').value
     : null
     let url = req.url;
-    if(!cook && url.includes(`/users/`)){
-        return NextResponse.redirect("https://esionn.netlify.app");
-    }
-    else if((cook && url === "https://esionn.netlify.app") || (cook && url ==="https://esionn.netlify.app/User")){
-        return NextResponse.redirect(`https://esionn.netlify.app/users/${id}`)
-    }
+    console.log(typeof(res.headers.get('Set-Cookie'))); // Will output `referer` header value
+
+    // if(!cook && url.includes(`/users/`)){
+    //     return NextResponse.redirect("https://esionn.netlify.app");
+    // }
+    // else if((cook && url === "https://esionn.netlify.app") || (cook && url ==="https://esionn.netlify.app/User")){
+    //     return NextResponse.redirect(`https://esionn.netlify.app/users/${id}`)
+    // }
 }

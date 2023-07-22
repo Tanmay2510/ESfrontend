c377f6eb50fb74914e0654375a8d4ee2a2f68179
Tanmay2510 <comparisionthing@gmail.com>
2023-07-23 04:27:05 +0530

    anim

--- component/middle/Sample.jsx
+++ component/middle/Sample.jsx
@@ -23,7 +23,7 @@ function Sample() {
     setNewUser();
   }
   const variants={
-    hidden: { opacity: 0, y: -75 },
+    hidden: { opacity: 0, y: +75 },
     visible: { opacity: 1, y: 0 },
   }
   return (
